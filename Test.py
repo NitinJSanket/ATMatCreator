eef7869a5815fc7c1ebce91e85365f38d735aaf7
NitinJSanket <firenitin@gmail.com>
2017-08-09 19:35:59 -0400

    Trying to add command Line parser

--- Test.py
+++ Test.py
@@ -2,12 +2,19 @@
 
 from wand.image import Image
 from wand.color import Color
+import argparse
 
 # Add command line args for parsing number of columns and rows
+parser = argparse.ArgumentParser()
+parser.add_argument("TagSize", help="size of the April Tag in m", type=int)
+args = parser.parse_args()
+print(args)
 
 # Create Random Scribble on image
 
 # Converting first page into PNG
+# Scale resolution as size
+
 with Image(filename="./alltags.pdf[0,1]", resolution=300) as img:
      img.format = 'png'
      img.background_color = Color("white")
